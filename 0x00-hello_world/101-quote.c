eec9aa2f73420db7d8e15e9c7bc56ab81072dc25
bimbolaar <rahmat.bimbola@gmail.com>
2023-04-24 01:49:41 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
-
+#include <stdio.h>
 /**
- * main - print without using printf and puts
- * Return : 1 (Success)
+ * main - use write to print quote instead of printf and puts
+ * Return: 1 (Success)
  */
 
 int main(void)
 
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
